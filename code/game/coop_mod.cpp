635d79430cecb440ebbf9b6023f74ccd4340e447
chrissstrahl <Chrissstrahl@users.noreply.github.com>
2022-09-19 00:39:08 +0200

    removed test entity

--- code/game/coop_mod.cpp
+++
@@ -1,6 +0,0 @@
-/*****************************************************************************/
-/*QUAKED coop_test (0 0.5 0) (-40 -40 -0) (40 40 32) i_love_you_all kill_merkel burn_schulz peace_on_earth i_love_you_all kill_merkel burn_schulz peace_on_earth
-COOP TEST ENTITY - HZM Coop Mod is required
-
-if i_love_you_all is set you will love everyone
-*/
